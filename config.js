879fc12a03434665c400aba5368849242a3a2b3b
abuzarhusna0 <ahgaming556677@gmail.com>
2025-07-29 12:28:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923130948950",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923367020490",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "X6-AbuXar",
 // add bot owner name
